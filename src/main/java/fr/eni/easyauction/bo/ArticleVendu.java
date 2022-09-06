b182874bf5451b617c7e8ecdf979d2eabadb456c
mrousseau2022 <mrousseau2022@47SE35-2Z7TT72.ad.campus-eni.fr>
2022-09-06 09:18:18 +0200

    Suppression des lignes setters getters

--- src/main/java/fr/eni/easyauction/bo/ArticleVendu.java
+++ src/main/java/fr/eni/easyauction/bo/ArticleVendu.java
@@ -29,183 +29,99 @@ public class ArticleVendu {
 	private List<Enchere> ListeEnchere = new ArrayList<Enchere>();
 	private Categorie categorie;
 	private Retrait[] retrait = new Retrait[1];
-	/**
-	 * Getter pour noArticle
-	 * 
-	 * @return the noArticle
-	 */
+	
 	public int getNoArticle() {
 		return noArticle;
 	}
-	/**
-	 * Setter pour noArticle
-	 * @param noArticle the noArticle to set
-	 */
+	
 	public void setNoArticle(int noArticle) {
 		this.noArticle = noArticle;
 	}
-	/**
-	 * Getter pour nomArticle
-	 * 
-	 * @return the nomArticle
-	 */
+	
 	public String getNomArticle() {
 		return nomArticle;
 	}
-	/**
-	 * Setter pour nomArticle
-	 * @param nomArticle the nomArticle to set
-	 */
+	
 	public void setNomArticle(String nomArticle) {
 		this.nomArticle = nomArticle;
 	}
-	/**
-	 * Getter pour description
-	 * 
-	 * @return the description
-	 */
+	
 	public String getDescription() {
 		return description;
 	}
-	/**
-	 * Setter pour description
-	 * @param description the description to set
-	 */
+	
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	/**
-	 * Getter pour dateDebutEncheres
-	 * 
-	 * @return the dateDebutEncheres
-	 */
+	
 	public LocalDate getDateDebutEncheres() {
 		return dateDebutEncheres;
 	}
-	/**
-	 * Setter pour dateDebutEncheres
-	 * @param dateDebutEncheres the dateDebutEncheres to set
-	 */
+	
 	public void setDateDebutEncheres(LocalDate dateDebutEncheres) {
 		this.dateDebutEncheres = dateDebutEncheres;
 	}
-	/**
-	 * Getter pour dateFinEncheres
-	 * 
-	 * @return the dateFinEncheres
-	 */
+	
 	public LocalDate getDateFinEncheres() {
 		return dateFinEncheres;
 	}
-	/**
-	 * Setter pour dateFinEncheres
-	 * @param dateFinEncheres the dateFinEncheres to set
-	 */
+	
 	public void setDateFinEncheres(LocalDate dateFinEncheres) {
 		this.dateFinEncheres = dateFinEncheres;
 	}
-	/**
-	 * Getter pour miseAPrix
-	 * 
-	 * @return the miseAPrix
-	 */
+	
 	public int getMiseAPrix() {
 		return miseAPrix;
 	}
-	/**
-	 * Setter pour miseAPrix
-	 * @param miseAPrix the miseAPrix to set
-	 */
+	
 	public void setMiseAPrix(int miseAPrix) {
 		this.miseAPrix = miseAPrix;
 	}
-	/**
-	 * Getter pour prixVente
-	 * 
-	 * @return the prixVente
-	 */
+	
 	public int getPrixVente() {
 		return prixVente;
 	}
-	/**
-	 * Setter pour prixVente
-	 * @param prixVente the prixVente to set
-	 */
+	
 	public void setPrixVente(int prixVente) {
 		this.prixVente = prixVente;
 	}
-	/**
-	 * Getter pour etatVente
-	 * 
-	 * @return the etatVente
-	 */
+	
 	public int getEtatVente() {
 		return etatVente;
 	}
-	/**
-	 * Setter pour etatVente
-	 * @param etatVente the etatVente to set
-	 */
+	
 	public void setEtatVente(int etatVente) {
 		this.etatVente = etatVente;
 	}
-	/**
-	 * Getter pour utilisateur
-	 * 
-	 * @return the utilisateur
-	 */
+	
 	public Utilisateur getUtilisateur() {
 		return utilisateur;
 	}
-	/**
-	 * Setter pour utilisateur
-	 * @param utilisateur the utilisateur to set
-	 */
+	
 	public void setUtilisateur(Utilisateur utilisateur) {
 		this.utilisateur = utilisateur;
 	}
-	/**
-	 * Getter pour listeEnchere
-	 * 
-	 * @return the listeEnchere
-	 */
+	
 	public List<Enchere> getListeEnchere() {
 		return ListeEnchere;
 	}
-	/**
-	 * Setter pour listeEnchere
-	 * @param listeEnchere the listeEnchere to set
-	 */
+	
 	public void setListeEnchere(List<Enchere> listeEnchere) {
 		ListeEnchere = listeEnchere;
 	}
-	/**
-	 * Getter pour categorie
-	 * 
-	 * @return the categorie
-	 */
+	
 	public Categorie getCategorie() {
 		return categorie;
 	}
-	/**
-	 * Setter pour categorie
-	 * @param categorie the categorie to set
-	 */
+	
 	public void setCategorie(Categorie categorie) {
 		this.categorie = categorie;
 	}
-	/**
-	 * Getter pour retrait
-	 * 
-	 * @return the retrait
-	 */
+	
 	public Retrait[] getRetrait() {
 		return retrait;
 	}
-	/**
-	 * Setter pour retrait
-	 * @param retrait the retrait to set
-	 */
+	
 	public void setRetrait(Retrait[] retrait) {
 		this.retrait = retrait;
 	}
