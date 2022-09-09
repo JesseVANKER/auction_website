3c861c5ee5789dd0fe9cabed7fd95933c9173434
mrousseau2022 <mrousseau2022@47SE35-2Z7TT72.ad.campus-eni.fr>
2022-09-09 09:32:39 +0200

    commit 25 09/09

--- src/main/java/fr/eni/easyauction/servlets/ServletAccueil.java
+++ src/main/java/fr/eni/easyauction/servlets/ServletAccueil.java
@@ -2,10 +2,7 @@ package fr.eni.easyauction.servlets;
 
 import java.io.IOException;
 
-
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,10 +17,8 @@ import javax.servlet.http.HttpSession;
 import fr.eni.easyauction.BusinessException;
 import fr.eni.easyauction.bll.EasyAuctionManager;
 import fr.eni.easyauction.bo.ArticleVendu;
-import fr.eni.easyauction.bo.Categorie;
 import fr.eni.easyauction.bo.Enchere;
 import fr.eni.easyauction.bo.Utilisateur;
-
 
 
 
@@ -39,89 +34,31 @@ public class ServletAccueil extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		List<Integer> listeCodesErreur=new ArrayList<>();
 		EasyAuctionManager easyAuctionManager = new EasyAuctionManager();
 		List<ArticleVendu> listeArticleVendu=null;
-		List<ArticleVendu> listeArticleRecherche=new ArrayList<ArticleVendu>();
-		List<Categorie> listeCategorie=null;
-		List<ArticleVendu> listeArticleCategorie=new ArrayList<ArticleVendu>();
-		List<ArticleVendu> listeArticleRechercheEtCategorie=new ArrayList<ArticleVendu>();
-		int idCategorie = 0;
+		List<Utilisateur> listeUtilisateur=null;
 		try {
 			listeArticleVendu = easyAuctionManager.selectionnerTousLesArticles();
-			listeCategorie = easyAuctionManager.selectionnerToutesCategories();
-			if(request.getParameter("recherche")!=null) {
-				
-			
-			for(ArticleVendu article : listeArticleVendu) {
-				if(article.getNomArticle().toLowerCase().contains(request.getParameter("recherche").toLowerCase().trim())) {
-					listeArticleRecherche.add(article);
-					
-					
-				}
-			}
-			request.setAttribute("listeArticleRecherche", listeArticleRecherche);
-			
-			}
-			if(request.getParameter("categorie")!=null && request.getParameter("categorie")!=("- Toutes les catégories -")) {
-				
-				for(Categorie categorie : listeCategorie) {
-					if(categorie.getLibelle().equals(request.getParameter("categorie"))) {
-						
-						idCategorie = categorie.getNoCategorie();
-					}
-				}
-				
-				listeArticleCategorie = easyAuctionManager.selectionnerTousLesArticlesByCategorie(idCategorie);
-				
-				
-				request.setAttribute("listeArticleCategorie", listeArticleCategorie);
-				}
-			
-			if(request.getParameter("categorie")!=null &&request.getParameter("recherche")!=null) {
-				for(ArticleVendu article : listeArticleVendu) {
-					if(article.getNomArticle().toLowerCase().contains(request.getParameter("recherche").toLowerCase())) {
-						for(Categorie categorie : listeCategorie) {
-							if(categorie.getLibelle().equals(request.getParameter("categorie"))) {
-								
-								idCategorie = categorie.getNoCategorie();
-							}
-						}
-						
-						listeArticleRechercheEtCategorie = easyAuctionManager.selectionnerTousLesArticlesByCategorie(idCategorie);
-						
-						
-						request.setAttribute("listeArticleRechercheEtCategorie", listeArticleRechercheEtCategorie);
-						
-						
-					}
-				}
-			}
-			
+			listeUtilisateur = easyAuctionManager.selectionnerTousLesUtilisateurs();
 				
 		} catch (BusinessException e) {
 			e.printStackTrace();
 			request.setAttribute("listeCodesErreur",e.getListeCodesErreur());
 		}
 		
-
-		
-		request.setAttribute("listeCategories", listeCategorie);
 		request.setAttribute("listeArticleVendu", listeArticleVendu);
-		
 
 
+		HttpSession session = request.getSession();
 		
 		
 
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/accueil.jsp");
 		
 		rd.forward(request, response);
-		
 
 	}
 
